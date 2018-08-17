c661801336afc0bf7e82f98d732b489118484194
Geoffrey Wu <geoffrey@wisenet.co>
2018-08-17 15:37:38 +1000

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,11 @@ class Quiz extends Component {
             quiz_position: 1,
         }
     }
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        })
+    }
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return (
@@ -18,7 +23,10 @@ class Quiz extends Component {
                 {isQuizEnd ? 
                     <QuizEnd /> 
                     : 
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                    <QuizQuestion 
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)} 
+                    />
                 }
             </div>
         )
